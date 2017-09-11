6f649bd59ab16c7efcbe69410e3d193d8012fee9
omar <omm27@hotmail.com>
2017-09-11 10:59:21 -0500

    se agrego las clases

---
+++ Cuestionario.java
@@ -0,0 +1,5 @@
+public class Cuestionario
+{
+    public void aplicaPregunta(Pregunta p)
+    {}
+}
---
+++ Pregunta.java
@@ -0,0 +1,24 @@
+public class Pregunta
+{
+    private String texto;
+    private String respuesta;
+    
+    public Pregunta()
+    {
+        
+    }
+    
+    public void asingarTexto(String pregunta)
+    {}
+    
+    public void asiganarRespuesta(String Respuesta)
+    {}
+    
+    public boolean checarRespuesta(String respuestausuario)
+    {
+        return true;
+    }
+    
+    public void imprimete()
+    {}
+}
